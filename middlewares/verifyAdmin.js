0c326fcc20e6049044967dc5b4404cfdcd4997e4
Negronii <478258625@qq.com>
2022-09-27 12:31:30 +1000

    add verify admin

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,9 +1,11 @@
 const User = require("../models/user");
 
-async function verifyToken(req, res, next) {
+async function verifyAdmin(req, res, next) {
     const user = await User.findOne({email: req.email});
     if (user.role !== "admin") {
         return res.status(401).json({msg: "you are not administrator!"});
     }
     next();
 }
+
+module.exports = {verifyAdmin}
