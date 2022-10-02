3bb0e5cf6fbb76763128b0f8e7836a152d4cc8c6
Dennis <honor1016dd@gmail.com>
2022-10-03 03:28:26 +1100

    Reformat

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,24 +1,23 @@
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith('Bearer')) {
+		return res.status(400).json({ msg: 'no token provided' });
+	}
+	const token = authHeader.split(' ')[1];
 
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer')) {
-        return res.status(400).json({msg: "no token provided"});
-    }
-    const token = authHeader.split(' ')[1];
-
-    try {
-        const payload = jwt.verify(token, process.env.TOKEN_SIGNATURE);
-        // attach the user to the job routes
-        req.email = payload.email;
-        next();
-    } catch (error) {
-        return res.status(401).json({msg: error.message});
-    }
+	try {
+		const payload = jwt.verify(token, process.env.TOKEN_SIGNATURE);
+		// attach the user to the job routes
+		req.email = payload.email;
+		next();
+	} catch (error) {
+		return res.status(401).json({ msg: error.message });
+	}
 }
 
 module.exports = {
-    verifyToken
-}
+	verifyToken,
+};
