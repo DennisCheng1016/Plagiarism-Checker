4e6f0217a8ec36f22a452c24ccff4ff0aa060f30
Dennis <honor1016dd@gmail.com>
2022-10-03 02:46:27 +1100

    Add error-handler and not-found middlewares and errors

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const authRouter = require('./routes/authRouter');
-const verifyToken = require('./middlewares/verifyToken').verifyToken;
-const cors = require('cors');
-const fileUpload = require("express-fileupload");
-const fileRouter = require("./routes/fileRouter");
-const checkerRouter = require("./routes/checkerRouter");
-const resultRouter = require("./routes/resultRouter");
-const subjectRouter = require("./routes/subjectRouter");
-const assignmentRouter = require("./routes/assignmentRouter");
-const bufferFileRouter = require("./routes/bufferFileRouter")
-const userRouter = require("./routes/userRouter");
-const adminRouter = require('./routes/adminRouter');
-const verifyAdmin = require('./middlewares/verifyAdmin').verifyAdmin;
-const { default: mongoose } = require('mongoose');
-const connectDB = require('./Config/connectMongo');
 require('dotenv').config();
-// console.log(process.env)
-
-//connect to database
-connectDB();
-
+require('express-async-errors');
+// Express
+const express = require('express');
 const app = express();
+// Database
+const connectDB = require('./Config/connectMongo');
+const { default: mongoose } = require('mongoose');
+
+// Rest of packages
+const fileUpload = require('express-fileupload');
+
+// Security
+const cors = require('cors');
+
+// Routers
+const authRouter = require('./routes/authRouter');
+const fileRouter = require('./routes/fileRouter');
+const checkerRouter = require('./routes/checkerRouter');
+const resultRouter = require('./routes/resultRouter');
+const subjectRouter = require('./routes/subjectRouter');
+const assignmentRouter = require('./routes/assignmentRouter');
+const bufferFileRouter = require('./routes/bufferFileRouter');
+const userRouter = require('./routes/userRouter');
+const adminRouter = require('./routes/adminRouter');
+
+// Middlewares
+const verifyAdmin = require('./middlewares/verifyAdmin').verifyAdmin;
+const verifyToken = require('./middlewares/verifyToken').verifyToken;
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+//connect to database
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,30 +38,34 @@ app.use(fileUpload());
 
 const port = process.env.PORT || 8888;
 
-app.get("/", (req, res) => {
-    res.send("SC-Quokka presents");
+app.get('/', (req, res) => {
+	res.send('SC-Quokka presents');
 });
 
-app.use("/auth", authRouter);
+app.use('/auth', authRouter);
 app.use(verifyToken);
-app.use("/user",userRouter);
-app.use("/file", fileRouter);
-app.use("/check", checkerRouter);
-app.use("/result", resultRouter);
-app.use("/subject",  subjectRouter);
-app.use("/assignment",  assignmentRouter);
-app.use("/buffer",  bufferFileRouter);
+app.use('/user', userRouter);
+app.use('/file', fileRouter);
+app.use('/check', checkerRouter);
+app.use('/result', resultRouter);
+app.use('/subject', subjectRouter);
+app.use('/assignment', assignmentRouter);
+app.use('/buffer', bufferFileRouter);
 app.use(verifyAdmin);
-app.use("/admin", adminRouter);
+app.use('/admin', adminRouter);
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+connectDB();
 mongoose.connection.once('open', () => {
-    console.log("Connected to MongoDB");
-    app.listen(port, (error) => {
-        if (!error)
-            console.log("Server is Successfully Running, and App is listening on port " + port);
-        else
-            console.log("Error occurred, server can't start", error);
-    }
-    );
+	console.log('Connected to MongoDB');
+	app.listen(port, error => {
+		if (!error)
+			console.log(
+				'Server is Successfully Running, and App is listening on port ' +
+					port
+			);
+		else console.log("Error occurred, server can't start", error);
+	});
 });
-
