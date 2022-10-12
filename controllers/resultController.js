e6ce768f54f6d6d65dd5ee5eac112c4161396115
Dennis <honor1016dd@gmail.com>
2022-10-12 21:02:01 +1100

    Refactor delete feature in each controllers in order to delete files in buffer and dataset correctly

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -4,42 +4,58 @@ const Dataset = require('../models/dataset');
 const Historical = require('../models/historical');
 const { StatusCodes } = require('http-status-codes');
 
+const getResultList = async (req, res) => {
+	console.log(req.params.id);
+	const checker = await User.findOne({ email: req.email }, {});
+	const resultList = await Result.find(
+		{ checker: checker.id, assignmentId: req.params.id },
+		{
+			fileName: true,
+			assignmentId: true,
+			similarity: true,
+			submitter: true,
+			when: true,
+		}
+	)
+		.populate({
+			path: 'assignmentId',
+			select: 'assignmentName',
+		})
+		.populate({
+			path: 'submitter',
+			select: 'username',
+		});
+	res.status(StatusCodes.OK).json(resultList);
+};
 
-const getResultList = async(req, res) => {
-    console.log(req.params.id);
-    const checker = await User.findOne({email:req.email},{});
-    const resultList = await Result.find({checker: checker.id, assignmentId: req.params.id}, {fileName:true, assignmentId:true, similarity:true, submitter:true, when:true}).lean();
-    res.status(StatusCodes.OK).send(JSON.stringify(resultList));
-}
+const getResultDetail = async (req, res) => {
+	const resultDetail = await Result.findOne({ id: req.params.id }, {}).lean();
+	res.status(StatusCodes.OK).send(JSON.stringify(resultDetail));
+};
 
-const getResultDetail = async(req, res) => {
-    const resultDetail = await Result.findOne({id:req.params.id}, {}).lean();
-    res.status(StatusCodes.OK).send(JSON.stringify(resultDetail));
-}
-
-const saveResultsToHistorical = async(req, res) => {
-    for (let i = 0; i < req.body.results.length; i++) {
-        let result = await Result.findOne({id:req.body.results[i]},{});
-        const historical = await Historical.create({
-            fileName: result.fileName,
-            assignmentId: result.assignmentId,
-            checker: result.checker,
-            submitter: result.submitter,
-            grade: req.body.grade,
-            datasets: req.body.datasets,
-            similarity: result.similarity,
-            duplicates: result.duplicates,
-            when: result.when,
-            textContent: result.textContent,
-            originalFile: result.originalFile
-        });
-        await result.remove();
-    }
-    res.status(StatusCodes.OK).send();
-}
+const saveResultsToHistorical = async (req, res) => {
+	for (let i = 0; i < req.body.results.length; i++) {
+		let result = await Result.findOne({ id: req.body.results[i] }, {});
+		const historical = await Historical.create({
+			fileName: result.fileName,
+			assignmentId: result.assignmentId,
+			checker: result.checker,
+			submitter: result.submitter,
+			grade: req.body.grade,
+			datasetId: req.body.datasetId,
+			similarity: result.similarity,
+			duplicates: result.duplicates,
+			when: result.when,
+			textContent: result.textContent,
+			originalFile: result.originalFile,
+		});
+		await result.remove();
+	}
+	res.status(StatusCodes.OK).send();
+};
 
 module.exports = {
-    getResultList,
-    getResultDetail,
-    saveResultsToHistorical
-}
+	getResultList,
+	getResultDetail,
+	saveResultsToHistorical,
+};
--- models/historical.js
+++ models/historical.js
@@ -1,68 +1,65 @@
 // require mongoose module
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 /* -------------------------------------- MODEL -------------------------------------- */
-const historicalSchema = new mongoose.Schema({
+const historicalSchema = new mongoose.Schema(
+	{
+		fileName: {
+			type: String,
+			required: true,
+		},
+		assignmentId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Assignment',
+			required: [true, 'Please provide assignment Id'],
+		},
+		checker: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: [true, 'Please provide user Id'],
+		},
+		submitter: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: [true, 'Please provide user Id'],
+		},
+		grade: {
+			type: String,
+			enum: ['passed', 'failed'],
+			required: true,
+		},
+		datasetId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Dataset',
+			required: [true, 'Please provide dataset Id'],
+		},
+		similarity: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		duplicates: {
+			type: [[String]],
+			required: false,
+			default: [],
+		},
+		when: {
+			type: Date,
+			required: true,
+			default: Date.now(),
+		},
+		textContent: {
+			type: String,
+			required: true,
+		},
+		originalFile: {
+			type: Buffer,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
 
-    fileName: {
-        type: String,
-        required: true
-    },
-    assignmentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Assignment',
-        required: [true, 'Please provide assignment Id']
-    },
-    checker : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide user Id']
-    },
-    submitter : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide user Id']
-    },
-    grade : {
-        type: String,
-        enum: ['passed', 'failed'],
-        required: true,
-    },
-    datasets : {
-        type: [
-            mongoose.Schema.Types.ObjectId
-        ],
-        ref: 'Dataset',
-        default: []
-    },
-    similarity: {
-        type: Number,
-        required: true,
-        default : 0
-    },
-    duplicates: {
-        type: [[String]],
-        required: false,
-        default : []
-    },
-    when: {
-        type: Date,
-        required: true,
-        default: Date.now()
-    },
-    textContent : {
-        type: String,
-        required: true
-    },
-    originalFile: {
-        type: Buffer,
-        required: true
-    }
-},
-    { timestamps: true }
-)
+const Historical = mongoose.model('Historical', historicalSchema);
 
-
-const Historical = mongoose.model("Historical", historicalSchema)
-
-module.exports = Historical
+module.exports = Historical;
