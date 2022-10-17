5eccda8f3550058efe184962493801427fa8c560
Dennis <honor1016dd@gmail.com>
2022-10-18 02:34:52 +1100

    Fix user test bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,44 @@
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 // a function for front-end to get user information from token
 async function getUserInfo(req, res) {
+	// get the user by email
+	let user = req.user;
 
-    // get the user by email
-    let user = req.user;
-
-    // sent message to front-end
-    res.status(201).json({
-        username: user.username,
-        email: user.email,
-        role: user.role,
-        subjects: user.subjects,
-        accountStatus: user.accountStatus
-    });
+	// sent message to front-end
+	res.status(201).json({
+		username: user.username,
+		email: user.email,
+		role: user.role,
+		subjects: user.subjects,
+		accountStatus: user.accountStatus,
+	});
 }
 
 // User login.
 async function resetPassword(req, res) {
+	const email = req.email;
+	const password = req.body.password;
 
-    const email = req.email;
-    const password = req.body.password;
+	// Find the user.
+	const user = await User.findOne({ email });
 
-    // Find the user.
-    const user = await User.findOne({email});
+	// If the user isn't found.
+	if (!user) {
+		throw new Error('User not found');
+	}
+	if (user.accountStatus === 'disabled') {
+		throw new Error('Your account has been disabled by administrator');
+	}
 
-    // If the user isn't found.
-    if (!user) {
-        throw new Error('User not found');
-    }
-    if (user.accountStatus === 'disabled') {
-        throw new Error('Your account has been disabled by administrator');
-    }
+	// hash the password
+	user.password = bcrypt.hashSync(password, 10);
+	await user.save();
 
-    // hash the password
-    user.password= await bcrypt.hashSync(password, 10);
-    user.save();
-
-    res.status(201).json({
-        msg: "Success"
-    });
+	res.status(201).json({
+		msg: 'Success',
+	});
 }
 
-module.exports = {getUserInfo, resetPassword}
+module.exports = { getUserInfo, resetPassword };
--- test/user.test.js
+++ test/user.test.js
@@ -1,44 +1,50 @@
 require('dotenv').config();
-const User = require("../models/user");
-const httpMocks = require("node-mocks-http");
+const User = require('../models/user');
+const httpMocks = require('node-mocks-http');
 const userController = require('../controllers/userController');
-const connectDB = require("../Config/connectMongo");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const connectDB = require('../Config/connectMongo');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 describe('checkAuth', () => {
-    beforeAll(async () => {
-        await connectDB();
-        // hash the password
-        const HashedPassword = await bcrypt.hashSync("Psw12345", 10);
-        await User.create({
-            username: "testStudent2", email: "testStudent2@test.com", password: HashedPassword, role:"student"
-        });
-    });
-    afterAll(async () => {
-        await User.findOneAndDelete({email: "testStudent2@test.com"});
-        await mongoose.disconnect();
-    });
-    // the verify token middleware will ensure all input are valid
-    it('getUserInfo (Success)', async () => {
-        const req = {};
-        // verify token will let req.user become user object
-        req.user = await User.findOne({email: "testStudent2@test.com"});
-        const res = httpMocks.createResponse();
-        await userController.getUserInfo(req, res);
-        expect(res.statusCode).toBe(201);
-        let data = JSON.parse(res._getData());
-        expect(data.email).toBe("testStudent2@test.com");
-    });
-    it('resetPassword (Success)', async () => {
-        const req = {email: "student1@gmail.com", body: {password: "123123"}};
-        // verify token will let req.user become user object
-        req.user = await User.findById('63439de4b703b94a4429086c');
-        const res = httpMocks.createResponse();
-        await userController.resetPassword(req, res);
-        expect(res.statusCode).toBe(201);
-        let data = JSON.parse(res._getData());
-        expect(data.msg).toBe("Success");
-    });
+	beforeAll(async () => {
+		await connectDB();
+		// hash the password
+		const HashedPassword = bcrypt.hashSync('Psw12345', 10);
+		await User.findOneAndDelete({ email: 'testStudent2@test.com' });
+		await User.create({
+			username: 'testStudent2',
+			email: 'testStudent2@test.com',
+			password: HashedPassword,
+			role: 'student',
+		});
+	});
+	afterAll(async () => {
+		await User.findOneAndDelete({ email: 'testStudent2@test.com' });
+		await mongoose.disconnect();
+	});
+	// the verify token middleware will ensure all input are valid
+	it('getUserInfo (Success)', async () => {
+		const req = {};
+		// verify token will let req.user become user object
+		req.user = await User.findOne({ email: 'testStudent2@test.com' });
+		const res = httpMocks.createResponse();
+		await userController.getUserInfo(req, res);
+		expect(res.statusCode).toBe(201);
+		let data = JSON.parse(res._getData());
+		expect(data.email).toBe('testStudent2@test.com');
+	});
+	it('resetPassword (Success)', async () => {
+		const req = {
+			email: 'testStudent2@test.com',
+			body: { password: '123123' },
+		};
+		// verify token will let req.user become user object
+		req.user = await User.findOne({ email: 'testStudent2@test.com' });
+		const res = httpMocks.createResponse();
+		await userController.resetPassword(req, res);
+		expect(res.statusCode).toBe(201);
+		let data = JSON.parse(res._getData());
+		expect(data.msg).toBe('Success');
+	});
 });
-
